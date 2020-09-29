7c79251f681bb5e4a2e1b5e4a916cb949bd0a7e6
Helton Isac <helton.isac@gmail.com>
2020-09-29 20:20:29 -0300

    calling the car api

--- Carangas/Listing/Controller/CarsTableViewController.swift
+++ Carangas/Listing/Controller/CarsTableViewController.swift
@@ -8,23 +8,35 @@
 import UIKit
 
 class CarsTableViewController: UITableViewController {
-
+    // MARK: - Properties
+    var cars: [Car] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        loadCars()
+    }
+    
+    // MARK: - Methods
+    private func loadCars() {
+        CarAPI().loadCars()
     }
     
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return cars.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let car = cars[indexPath.row]
+        cell.textLabel?.text = car.name
+        cell.detailTextLabel?.text = car.brand
         return cell
     }
 }
