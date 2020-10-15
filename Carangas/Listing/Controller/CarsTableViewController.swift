51bd7d1319c9ea55c0d0f89f43e62376051f286b
Helton Isac <helton.isac@gmail.com>
2020-10-15 20:46:42 -0300

    Implementing mvvm for CarListing

--- Carangas/Listing/Controller/CarsTableViewController.swift
+++ Carangas/Listing/Controller/CarsTableViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class CarsTableViewController: UITableViewController {
+    
     // MARK: - Properties
-    var cars: [Car] = []
-
+    var viewModel = CarsListingViewModel()
+    
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.carsDidUpdate = carsDidUpdate
         refreshControl?.addTarget(self, action: #selector(loadCars), for: .valueChanged)
     }
     
@@ -30,63 +32,41 @@ class CarsTableViewController: UITableViewController {
     
     // MARK: - Methods
     @objc private func loadCars() {
-        
-        //GEITO VIDA LOKA
-        /*
-        URLSession.shared.dataTask(with: URL(string: "https://carangas.herokuapp.com/cars")!) { (data, _, _) in
-            self.cars = try! JSONDecoder().decode([Car].self, from: data!)
-            DispatchQueue.main.async {self.tableView.reloadData()}
-        }.resume()
-        */
-        
-
-        CarAPI().loadCars { [weak self] (result) in
-            guard let self = self else {return}
-            switch result {
-            case .success(let cars):
-                self.cars = cars
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            case .failure(let apiError):
-                switch apiError {
-                case .badURL:
-                    print("URL inválida")
-                default:
-                    print("Outro cenário de erro")
-                }
-            }
-            DispatchQueue.main.async {
-                self.refreshControl?.endRefreshing()
-            }
+        viewModel.loadCars()
+    }
+    
+    private func carsDidUpdate() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
         }
     }
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cars.count
+        return viewModel.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let car = cars[indexPath.row]
-        cell.textLabel?.text = car.name
-        cell.detailTextLabel?.text = car.brand
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CarTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: viewModel.cellViewModelFor(indexPath))
         return cell
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let car = cars[indexPath.row]
-            CarAPI().deleteCar(car) { [weak self] (result) in
+            viewModel.deleteCar(at: indexPath) { (result) in
                 switch result {
                 case .success:
-                    self?.cars.remove(at: indexPath.row)
+                    break
+                case .failure(let apiError):
                     DispatchQueue.main.async {
-                        tableView.deleteRows(at: [indexPath], with: .automatic)
+                        Alert.show(title: "Erro",
+                                   message: "Não foi possível excluir o carro. Motivo: \(apiError.errorMessage)",
+                                   presenter: self)
                     }
-                case .failure:
-                    print("Errrroooooooooou!!!")
                 }
             }
         }
---
+++ Carangas/Listing/ViewModel/CarCellViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CarCellViewModel.swift
+//  Carangas
+//
+//  Created by Helton Isac Torres Galindo on 15/10/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import Foundation
+
+struct CarCellViewModel: VehicleCellViewModelProtocol {
+    
+    private var car: Car
+    
+    init(car: Car) {
+        self.car = car
+    }
+    
+    var name: String {
+        car.name
+    }
+    
+    var brand: String {
+        car.brand.uppercased()
+    }
+}
