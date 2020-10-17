661aaf6c662fc51aa3e2ce7b728b56f2c1a13b32
Helton Isac <helton.isac@gmail.com>
2020-10-17 09:22:13 -0300

    Removing storyboard, using coordinator

---
+++ Carangas/Main/Coordinator/AppCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  AppCoordinator.swift
+//  Carangas
+//
+//  Created by Helton Isac Torres Galindo on 17/10/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinators: Coordinator?
+    
+    init() {
+        navigationController = UINavigationController()
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.tintColor = UIColor(named: "main")
+    }
+    
+    func start() {
+        
+    }
+}
