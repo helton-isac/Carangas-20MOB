d2e41c339e54cb837c7426ec843e5f764149e802
Helton Isac <helton.isac@gmail.com>
2020-10-17 09:31:42 -0300

    Refactoring StoryBoards Id

---
+++ Carangas/Listing/Coordinator/CarsListingCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  CarsListingCoordinator.swift
+//  Carangas
+//
+//  Created by Helton Isac Torres Galindo on 17/10/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class CarsListingCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var parentCoordinators: Coordinator?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+}
