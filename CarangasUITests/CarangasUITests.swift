eba3913d61ec36e9862a593b331fbe45c9c266a2
Helton Isac <helton.isac@gmail.com>
2020-10-22 22:09:01 -0300

    Creating UiTests Target

---
+++ CarangasUITests/CarangasUITests.swift
@@ -0,0 +1,29 @@
+//
+//  CarangasUITests.swift
+//  CarangasUITests
+//
+//  Created by Helton Isac Torres Galindo on 22/10/20.
+//  Copyright © 2020 Eric Brito. All rights reserved.
+//
+
+import XCTest
+
+class CarangasUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+
+    }
+}
